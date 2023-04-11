00c6d8c8ceef385dac897cdf43cbb7adecf785ea
juanmagal <juanma.galmes@gmail.com>
2023-04-11 14:02:19 +0000

    Adding yaml files

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@
 #    under the License.
 
 # !/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+#"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
